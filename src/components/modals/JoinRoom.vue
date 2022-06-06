cf4cf24002df7a37671160a8b9bb3d9136cef412
Tryston Perry <trystonmperry@gmail.com>
2022-06-06 16:00:05 -0700

    Save username in localstorage

--- src/components/modals/JoinRoom.vue
+++ src/components/modals/JoinRoom.vue
@@ -2,12 +2,18 @@
   <div>
     <h1 class="text-2xl text-grey-400 mb-2">Join Room</h1>
     <form @submit.prevent="onSubmit">
-      <d-input v-model="username" label="Enter your name" required class="flex-grow mr-2 mb-4" />
+      <d-input
+        v-model="username"
+        label="Enter your name"
+        required
+        class="flex-grow mr-2 mb-4"
+      />
       <d-btn
         type="submit"
         variant="none"
         class="p-btn inline-block rounded font-bold bg-dark-2 px-3 py-2"
-      >Join Room</d-btn>
+        >Join Room</d-btn
+      >
     </form>
   </div>
 </template>
@@ -17,6 +23,14 @@ export default {
   data: () => ({
     username: ""
   }),
+
+  created() {
+    const username = localStorage.getItem("username");
+    if (username) {
+      this.username = username;
+      this.onSubmit();
+    }
+  },
 
   methods: {
     async onSubmit() {
@@ -31,11 +45,11 @@ export default {
         return;
       }
 
+      localStorage.setItem("username", this.username);
       this.$store.dispatch("nav/hideModal");
     }
   }
 };
 </script>
 
-<style>
-</style>
+<style></style>
